ad63bac1e79572e14dc28296045d651df676dc4a
ndhenry <nelsonhenryme@gmail.com>
2016-08-05 17:36:26 -0500

    Completed second pass primary files

--- projects/06/Assembler/Assembler.py
+++ projects/06/Assembler/Assembler.py
@@ -1,4 +1,4 @@
-import SymbolTable, Code, Parser, sys
+import SymbolTable, Code, Parser, sys, os.path
 
 
 class Assembler(object):
@@ -6,54 +6,83 @@ class Assembler(object):
     def __init__(self):
         self.symbols = SymbolTable.SymbolTable()
 
-    def pass1(self):
-        """Pass 1 is responsible for management of symbols and removal of unnecessary portions of the assembly code."""
-        pass
-
-    def pass2(self, sourcefile, outfile):
-        """Pass 2 is responsible for translation of prepared assembly code to HACK binary machine code."""
-
-        parser = Parser.Parser(sourcefile)
-        code = Code.Code()
-        outfile = open()
-        while Parser.has_more_commands():
-
-            parser.advance()
-            cmd = parser.command_type()
-
-            if cmd == parser.a_cmd:
-                pass
-
-            elif cmd == parser.c_cmd:
-                pass
-
-            elif cmd == parser.l_cmd:
-                pass
-
-            else:
-                print('Pass 2: Failed Check')
-
-        outfile.close()
-
     def outname(self, sourcefile):
         """Generates an appropriate output file name."""
         return sourcefile.replace('.asm', '.hack')
 
+    def pass1(self, sourcefile):
+        """Pass 1 is responsible for management of symbols and removal of unnecessary portions of the assembly code."""
+        parser = Parser.Parser(sourcefile)
+        with open(parser.sourcefile) as parser.open_file:
+            for line in parser.open_file:
+                print(line)
+
+        return parser
+
+    def pass2(self, parser):
+        """Pass 2 is responsible for translation of prepared assembly code to HACK binary machine code."""
+        code = Code.Code()
+
+        # Overwriting any existing file and preparing for
+        outfile = open(self.outname(parser.sourcefile),'w')
+
+        with open(parser.sourcefile) as parser.open_file:
+            for line in parser.open_file:
+
+                cmd = parser.command_type(line)
+                line = line.strip('\n')
+
+                if cmd == parser.a_cmd:
+                    address = parser.symbol(line)
+                    outfile.write((code.a_gen(address) + '\n'))
+
+                elif cmd == parser.c_cmd:
+                    parts = parser.c_dissect(line)
+                    outfile.write((code.c_gen(parts[0], parts[1], parts[2]) + '\n'))
+
+                elif cmd == parser.l_cmd:
+                    outfile.write('L Command\n')
+
+                else:
+                    print('Not A, C, or L Command')
+
+        outfile.close()
+
     def build(self, sourcefile):
         """Build is responsible for the management of the assembly process."""
 
-        self.pass1(sourcefile)
-        self.pass2(sourcefile, self.outname(sourcefile))
+        parser = self.pass1(sourcefile)
+        self.pass2(parser)
+
+
+def input_checking(args):
+    if len(args) != 2:
+        print('Format: python Assembler filename.asm')
+        exit()
+    else:
+        sourcefile = args[1]
+
+    if os.path.isfile(sourcefile.replace('.asm', '.hack')):
+        print('\nOutput file already exists, Override?')
+        response = ''
+        while response == '':
+            response = input('Y/N: ')
+            if (response == 'Y') | (response == 'y'):
+                continue
+            elif (response == 'N') | (response == 'n'):
+                exit()
+            else:
+                response = ''
+        print('')
+
+    return sourcefile
 
 
 def main():
-    if len(sys.argv) != 2:
-        print('Format: python Assembler filename.asm')
-    else:
-        sourcefile = sys.argv[1]
-
+    sourcefile = input_checking(sys.argv)
     assembler = Assembler()
     assembler.build(sourcefile)
-    print('Done')
+    print('\nDone\n')
 
-main()
+if __name__ == "__main__":
+    main()
--- projects/06/Assembler/Parser.py
+++ projects/06/Assembler/Parser.py
@@ -1,30 +1,47 @@
 import re
-import fileinput
+
 
 class Parser:
-    def __init__(self, ):
+    def __init__(self, sourcefile):
         self.a_cmd = 0
         self.c_cmd = 1
         self.l_cmd = 2
+        self.sourcefile = sourcefile
+        self.re_dest_reduce = '@.*'
+        self.re_comp_reduce = ''
+        self.re_jump_reduce = ''
         return
 
-    def has_more_commands(self):
-        return # Boolean true or false
+    def command_type(self, line):
+        """Determines the command type of a given assembly code line"""
+        if line.startswith('//'):
+            return None
+        elif line.startswith('@'):
+            return self.a_cmd
+        elif '=' in line or ';' in line:
+            return self.c_cmd
+        elif line.startswith('(') and line.endswith(')'):
+            return self.l_cmd
+        else:
+            if line != '\n':
+                print('Error: has_more_commands had no correct command matching for %r' % line)
 
-    def advance(self):
-        return # nothing, just advances the current line
+    def symbol(self, line):
+        """Returns a string of A or L command types"""
+        # Needs to address symbol conversion to addresses
+        return (re.search('(?<=@)\w+', line)).group(0)
 
-    def command_type(self):
-        return # Either: a_cmd, c_cmd, l_cmd
+    def c_dissect(self, line):
+        """Returns the Hack Mnemonics dissected into dest, comp, and jump sections"""
+        half1 = re.split('[=]', line)
+        if len(half1) == 2:
+            half1.append(' ')
+            parts = half1
 
-    def symbol(self):
-        return # String: returns if A or L command types
+        else:
+            half2 = re.split('[;]', line)
+            temp = [' ']
+            temp.extend(half2)
+            parts = temp
 
-    def dest(self):
-        return # String: the mnemonic destination representation
-
-    def comp(self):
-        return # String: the mnemonic command representation
-
-    def jump(self):
-        return # String: the mnemonic jump representation
+        return parts
